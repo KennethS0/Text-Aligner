af54d2a4167ce6cd2e84dc85f556d339b9445332
Kenneth Sánchez <38081826+KennethS0@users.noreply.github.com>
2020-10-19 16:48:53 -0600

    Update text-alligner.hs

--- text-alligner.hs
+++ text-alligner.hs
@@ -6,11 +6,31 @@ type Line = [Token]
 
 -- Token: Can take 3 forms Word ("abc"), "Blank" (" "), or "HypWord" ("abc-") 
 data Token = Word String | Blank | HypWord String
-             deriving (Eq,Show)
+             deriving (Eq, Show)
+
+-- Show for each different token
+showToken :: Token -> String
+showToken (Word(a)) = a ++ " "
+showToken (HypWord(a)) = a ++ "- " 
+showToken Blank = " "
+
+
+-- trim:
+-- Gets rid of trailing spaces
+trim :: String -> String
+trim x | head x == ' ' = trim (tail x)
+       | last x == ' ' = trim (init x)
+       | otherwise = x
+
 
 -- string2line: 
 -- Takes a string as input and returns a Line
 string2line :: String -> Line
-string2line "" = []
-string2line (char:chars) | char == ' ' = [Blank] ++ string2line chars
-                         | otherwise = [Word [char]] ++ string2line chars
+string2line str = [Word x | x <- words str]
+
+
+-- line2string
+-- Takes a line as input and returns a string
+line2string :: Line -> String
+line2string [] = ""
+line2string (x:xs) = showToken x ++ line2string xs
